88d8180153a39cf8860441dbec953b4d15a8cdf7
fostlt <lanceadamp@gmail.com>
2020-02-29 16:10:10 -0800

    Basic structure for vending machine

---
+++ __tests__/vendingMachine.spec.js
@@ -0,0 +1,13 @@
+const { VendingMachine } = require("../lib/VendingMachine");
+const inventory = require("../mockData.json");
+
+let vendingMachine = VendingMachine
+
+// Create a test for inventory
+describe("Vending Machine", () => {
+    beforeEach(() => {
+      
+    });
+});
+
+
