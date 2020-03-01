d69adf64b8e76273bbe04e5b2568ff96196d2098
fostlt <lanceadamp@gmail.com>
2020-02-29 17:14:49 -0800

    Shows current vending machine inventory

--- __tests__/vendingMachine.spec.js
+++ __tests__/vendingMachine.spec.js
@@ -1,13 +1,76 @@
 const { VendingMachine } = require("../lib/VendingMachine");
 const inventory = require("../mockData.json");
 
-let vendingMachine = VendingMachine
 
 // Create a test for inventory
 describe("Vending Machine", () => {
-    beforeEach(() => {
-      
+  beforeEach(() => {
+    vendingMachine = new VendingMachine(inventory);
+  });
+  describe("Show the inventory of the Vending Machine", () => {
+    it("will return the inventory of", () => {
+      expect(vendingMachine.currentInventory()).toEqual({
+        products: [
+          {
+            product: "Pepsi",
+            quantity: 10,
+            maxQuantity: 10,
+            cost: 1.5,
+            id: "A1"
+          },
+          {
+            product: "Flamin' Hot Cheetos",
+            quantity: 10,
+            maxQuantity: 10,
+            cost: 1.75,
+            id: "A2"
+          },
+          {
+            product: "Kit Kat",
+            quantity: 10,
+            maxQuantity: 10,
+            cost: 1.25,
+            id: "A3"
+          },
+          {
+            product: "Dr.Pepper",
+            quantity: 10,
+            maxQuantity: 10,
+            cost: 1.5,
+            id: "A4"
+          },
+          {
+            product: "Skittles",
+            quantity: 10,
+            maxQuantity: 10,
+            cost: 1,
+            id: "A5"
+          }
+        ],
+        coins: [
+          {
+            name: "quarter",
+            amount: 0.25,
+            quantity: 10,
+            minQuantity: 10,
+            id: 1
+          },
+          {
+            name: "loonie",
+            amount: 1.0,
+            quantity: 10,
+            minQuantity: 10,
+            id: 2
+          },
+          {
+            name: "toonie",
+            amount: 2.0,
+            quantity: 10,
+            minQuantity: 10,
+            id: 3
+          }
+        ]
+      });
     });
+  });
 });
-
-
